ffb65ec016e29bf5d9028415c30cfab2c130ec02
Kanapiya <n1ght.d1ck1ns@gmail.com>
2024-03-03 17:38:52 +0500

    added text field validation and fixed design

--- lab4/ViewController.swift
+++ lab4/ViewController.swift
@@ -7,17 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var pasCorrect = "pas123"
+
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var label: UILabel!
-
+    @IBOutlet weak var TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        TextField.delegate = self
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func button(_ sender: Any) {
         label.text = "button is pressed"
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField.text == pasCorrect {
+            label.text = "Correct password"
+        }
+        
+        return true
+    }
+
+    
+    
     
 }
